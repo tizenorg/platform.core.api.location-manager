d8ea5880f9e03d2391c9bb1b4839fa09ce8378ca
Young-Ae Kang <youngae.kang@samsung.com>
2015-06-10 21:09:59 +0900

    1. Code synchronization with tizen_2.4 2. Changed configuration to depend on lbs-location
    Signed-off-by: Young-Ae Kang <youngae.kang@samsung.com>
    Change-Id: Ife52faf55df91e872bdfc48824d0a29a76c85688

---
+++ src/location_batch.c
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2011-2013 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <system_info.h>
+#include "location_internal.h"
+#include "location_batch.h"
+
+
+/*
+* Internal Implementation
+*/
+
+static void __cb_batch_updated(GObject *self, guint num_of_location, gpointer userdata)
+{
+	LOCATIONS_LOGD("Batch callback function has been invoked.");
+	location_manager_s *handle = (location_manager_s *) userdata;
+
+	if (handle->user_cb[_LOCATIONS_EVENT_TYPE_BATCH]) {
+		((location_batch_cb) handle->user_cb[_LOCATIONS_EVENT_TYPE_BATCH])(num_of_location, handle->user_data[_LOCATIONS_EVENT_TYPE_BATCH]);
+	}
+}
+
+/*/////////////////////////////////////// */
+/* Location Manager */
+/*////////////////////////////////////// */
+
+EXPORT_API int location_manager_set_location_batch_cb(location_manager_h manager, location_batch_cb callback, int batch_interval, int batch_period, void *user_data)
+{
+	LOCATIONS_LOGD("location_manager_set_location_batch_cb");
+	LOCATIONS_NOT_SUPPORTED_CHECK(__is_location_supported());
+
+	LOCATIONS_CHECK_CONDITION(batch_interval >= 1 && batch_interval <= 120, LOCATIONS_ERROR_INVALID_PARAMETER, "LOCATIONS_ERROR_INVALID_PARAMETER");
+	LOCATIONS_CHECK_CONDITION(batch_period >= 120 && batch_period <= 600, LOCATIONS_ERROR_INVALID_PARAMETER, "LOCATIONS_ERROR_INVALID_PARAMETER");
+	LOCATIONS_NULL_ARG_CHECK(manager);
+	LOCATIONS_NULL_ARG_CHECK(callback);
+	location_manager_s *handle = (location_manager_s *) manager;
+	g_object_set(handle->object, "batch-period", batch_period, NULL);
+	g_object_set(handle->object, "batch-interval", batch_interval, NULL);
+	return __set_callback(_LOCATIONS_EVENT_TYPE_BATCH, manager, callback, user_data);
+}
+
+EXPORT_API int location_manager_unset_location_batch_cb(location_manager_h manager)
+{
+	LOCATIONS_LOGD("location_manager_unset_location_batch_cb");
+	LOCATIONS_NOT_SUPPORTED_CHECK(__is_location_supported());
+	return __unset_callback(_LOCATIONS_EVENT_TYPE_BATCH, manager);
+}
+
+EXPORT_API int location_manager_start_batch(location_manager_h manager)
+{
+	LOCATIONS_LOGD("location_manager_start_batch");
+	LOCATIONS_NOT_SUPPORTED_CHECK(__is_location_supported());
+	LOCATIONS_NULL_ARG_CHECK(manager);
+	location_manager_s *handle = (location_manager_s *) manager;
+
+	if (LOCATIONS_METHOD_GPS == handle->method) {
+		if (!handle->sig_id[_LOCATION_SIGNAL_BATCH_UPDATED]) {
+			handle->sig_id[_LOCATION_SIGNAL_BATCH_UPDATED] = g_signal_connect(handle->object, "batch-updated", G_CALLBACK(__cb_batch_updated), handle);
+		}
+	} else {
+		LOCATIONS_LOGE("method is not GPS");
+	}
+
+	if (handle->user_cb[_LOCATIONS_EVENT_TYPE_BATCH] != NULL) {
+		LOCATIONS_LOGI("batch status set : Start");
+	}
+
+	int ret = location_start_batch(handle->object);
+	if (ret != LOCATION_ERROR_NONE) {
+		return __convert_error_code(ret);
+	}
+
+	return LOCATIONS_ERROR_NONE;
+}
+
+EXPORT_API int location_manager_stop_batch(location_manager_h manager)
+{
+	LOCATIONS_LOGD("location_manager_stop_batch");
+	LOCATIONS_NOT_SUPPORTED_CHECK(__is_location_supported());
+	LOCATIONS_NULL_ARG_CHECK(manager);
+	location_manager_s *handle = (location_manager_s *) manager;
+
+	if (LOCATIONS_METHOD_GPS == handle->method) {
+		if (handle->sig_id[_LOCATION_SIGNAL_BATCH_UPDATED]) {
+			g_signal_handler_disconnect(handle->object, handle->sig_id[_LOCATION_SIGNAL_BATCH_UPDATED]);
+			handle->sig_id[_LOCATION_SIGNAL_BATCH_UPDATED] = 0;
+		}
+	} else {
+		LOCATIONS_LOGE("method is not GPS");
+	}
+
+	int ret = location_stop_batch(handle->object);
+	if (ret != LOCATION_ERROR_NONE) {
+		return __convert_error_code(ret);
+	}
+
+	return LOCATIONS_ERROR_NONE;
+}
+
+EXPORT_API int location_manager_foreach_location_batch(location_manager_h manager, location_batch_get_location_cb callback, void *user_data)
+{
+	LOCATIONS_LOGD("location_manager_foreach_location_batch");
+	LOCATIONS_NOT_SUPPORTED_CHECK(__is_location_supported());
+	LOCATIONS_NULL_ARG_CHECK(manager);
+	LOCATIONS_NULL_ARG_CHECK(callback);
+	location_manager_s *handle = (location_manager_s *) manager;
+	LocationBatch *batch = NULL;
+
+	int ret = location_get_batch(handle->object, &batch);
+	if (ret != LOCATION_ERROR_NONE || batch == NULL) {
+		if (ret == LOCATION_ERROR_NOT_SUPPORTED) {
+			LOCATIONS_LOGE("LOCATIONS_ERROR_INCORRECT_METHOD(0x%08x) : method - %d", LOCATIONS_ERROR_INCORRECT_METHOD, handle->method);
+			return LOCATIONS_ERROR_INCORRECT_METHOD;
+		} else if (ret == LOCATION_ERROR_NOT_ALLOWED) {
+			LOCATIONS_LOGE("LOCATIONS_ERROR_ACCESSIBILITY_NOT_ALLOWED");
+			return LOCATIONS_ERROR_ACCESSIBILITY_NOT_ALLOWED;
+		}
+
+		LOCATIONS_LOGE("LOCATIONS_ERROR_SERVICE_NOT_AVAILABLE(0x%08x) : batch is NULL ",
+		               LOCATIONS_ERROR_SERVICE_NOT_AVAILABLE);
+		return LOCATIONS_ERROR_SERVICE_NOT_AVAILABLE;
+	}
+
+	int i;
+	for (i = 0; i < batch->num_of_location; i++) {
+		gdouble latitude;
+		gdouble longitude;
+		gdouble altitude;
+		gdouble speed;
+		gdouble direction;
+		gdouble h_accuracy;
+		gdouble v_accuracy;
+		guint timestamp;
+
+		location_get_batch_details(batch, i, &latitude, &longitude, &altitude, &speed, &direction, &h_accuracy, &v_accuracy, &timestamp);
+		if (callback(latitude, longitude, altitude, speed, direction, h_accuracy, v_accuracy, timestamp, user_data) != TRUE) {
+			break;
+		}
+	}
+	location_batch_free(batch);
+	batch = NULL;
+	return LOCATIONS_ERROR_NONE;
+}
--- test/location_test.c
+++ test/location_test.c
@@ -21,15 +21,35 @@
 #include <locations.h>
 #include <location_bounds.h>
 
-location_manager_h manager;
+static location_manager_h manager;
+static GMainLoop *g_mainloop = NULL;
+static char *menu;
 
+static void location_cleanup();
+static int location_test();
+
+static gboolean exit_program(gpointer data)
+{
+	g_main_loop_quit(g_mainloop);
+
+	return FALSE;
+}
+
+static gboolean wait_test()
+{
+	location_cleanup();
+	location_test();
+
+	return FALSE;
+}
+
+#if 0
 void zone_event_cb(location_boundary_state_e state, double latitude, double longitude, double altitude, time_t timestamp,
-		   void *user_data)
+                   void *user_data)
 {
 	if (state == LOCATIONS_BOUNDARY_IN) {
 		printf("Entering zone\n");
-	} else			// state == LOCATIONS_BOUNDARY_OUT
-	{
+	} else {		/* state == LOCATIONS_BOUNDARY_OUT */
 		printf("Leaving zone\n");
 	}
 
@@ -38,19 +58,20 @@ void zone_event_cb(location_boundary_state_e state, double latitude, double long
 	printf("Time: %s\n", ctime(&timestamp));
 }
 
-static bool satellites_foreach_cb(unsigned int azimuth, unsigned int elevation, unsigned int prn, int snr, bool is_in_use,
-				  void *user_data)
+static bool last_satellites_foreach_cb(unsigned int azimuth, unsigned int elevation, unsigned int prn, int snr, bool is_in_use,
+                                       void *user_data)
 {
-	printf("[Satellite information]  azimuth  : %d, elevation : %d, prn :%d, snr : %d, used: %d\n", azimuth, elevation, prn,
-	       snr, is_in_use);
+	printf("[Last Satellite information]	azimuth	: %d, elevation: %d, prn: %d, snr: %d, used: %d\n", azimuth, elevation,
+	       prn, snr, is_in_use);
 	return true;
 }
+#endif
 
-static bool last_satellites_foreach_cb(unsigned int azimuth, unsigned int elevation, unsigned int prn, int snr, bool is_in_use,
-				       void *user_data)
+static bool satellites_foreach_cb(unsigned int azimuth, unsigned int elevation, unsigned int prn, int snr, bool is_in_use,
+                                  void *user_data)
 {
-	printf("[Last Satellite information]  azimuth  : %d, elevation : %d, prn :%d, snr : %d, used: %d\n", azimuth, elevation,
-	       prn, snr, is_in_use);
+	printf("[Satellite information]	azimuth	: %d, elevation: %d, prn: %d, snr: %d, used: %d\n", azimuth, elevation, prn,
+	       snr, is_in_use);
 	return true;
 }
 
@@ -66,7 +87,7 @@ static void _state_change_cb(location_service_state_e state, void *user_data)
 		time_t timestamp;
 		ret = location_manager_get_position(lm, &altitude, &latitude, &longitude, &timestamp);
 		if (ret != LOCATIONS_ERROR_NONE) {
-			printf(" Fail : location_manager_get_position ---> %d \n", ret);
+			printf(" Fail: location_manager_get_position ---> %d \n", ret);
 		} else {
 			printf("[%ld] alt: %g, lat %g, long %g\n", timestamp, altitude, latitude, longitude);
 		}
@@ -76,38 +97,42 @@ static void _state_change_cb(location_service_state_e state, void *user_data)
 		double vertical;
 		ret = location_manager_get_accuracy(lm, &level, &horizontal, &vertical);
 		if (ret != LOCATIONS_ERROR_NONE) {
-			printf(" Fail : location_manager_get_accuracy ---> %d \n", ret);
+			printf(" Fail: location_manager_get_accuracy ---> %d \n", ret);
 		} else {
-			printf("Level : %d, horizontal: %g, vertical %g\n", level, horizontal, vertical);
+			printf("Level: %d, horizontal: %g, vertical %g\n", level, horizontal, vertical);
 		}
 
 		char *nmea;
 		ret = gps_status_get_nmea(lm, &nmea);
 		if (ret != LOCATIONS_ERROR_NONE) {
-			printf(" Fail : gps_status_get_nmea ---> %d \n", ret);
+			printf(" Fail: gps_status_get_nmea ---> %d \n", ret);
 		} else {
-			printf("NMEA : %s\n", nmea);
+			printf("NMEA: %s\n", nmea);
 			free(nmea);
 		}
 
-		int num_of_view, num_of_active;
+		int num_of_view = 0, num_of_active = 0;
 		ret = gps_status_get_satellite(lm, &num_of_active, &num_of_view, &timestamp);
 		if (ret != LOCATIONS_ERROR_NONE) {
-			printf(" Fail : gps_status_get_satellite_count_in_view ---> %d \n", ret);
+			printf(" Fail: gps_status_get_satellite_count_in_view ---> %d \n", ret);
 		} else {
-			printf("[%ld] Satellite number of active : %d, in view : %d\n", timestamp, num_of_active, num_of_view);
+			printf("[%ld] Satellite number of active: %d, in view: %d\n", timestamp, num_of_active, num_of_view);
 		}
 
-		ret = gps_status_foreach_satellites_in_view(lm, satellites_foreach_cb, user_data);
-		if (ret != LOCATIONS_ERROR_NONE) {
-			printf(" Fail : gps_status_foreach_satellites_in_view ---> %d \n", ret);
+		if (num_of_view) {
+			ret = gps_status_foreach_satellites_in_view(lm, satellites_foreach_cb, user_data);
+			if (ret != LOCATIONS_ERROR_NONE) {
+				printf(" Fail: gps_status_foreach_satellites_in_view ---> %d \n", ret);
+			}
 		}
+		g_timeout_add_seconds(1, wait_test, NULL);
 	}
 }
 
+#if 0
 static bool __poly_coords_cb(location_coords_s coords, void *user_data)
 {
-	printf("location_bounds_foreach_rect_coords(latitude : %lf, longitude: %lf) \n", coords.latitude, coords.longitude);
+	printf("location_bounds_foreach_rect_coords(latitude: %lf, longitude: %lf) \n", coords.latitude, coords.longitude);
 	return TRUE;
 }
 
@@ -122,14 +147,14 @@ static bool __location_bounds_cb(location_bounds_h bounds, void *user_data)
 			location_coords_s center;
 			double radius;
 			location_bounds_get_circle_coords(bounds, &center, &radius);
-			printf("location_bounds_get_circle_coords(center : %lf, %lf, radius : %lf) \n", center.latitude,
+			printf("location_bounds_get_circle_coords(center: %lf, %lf, radius: %lf) \n", center.latitude,
 			       center.longitude, radius);
 
 		} else if (type == LOCATION_BOUNDS_RECT) {
 			location_coords_s left_top;
 			location_coords_s right_bottom;
 			location_bounds_get_rect_coords(bounds, &left_top, &right_bottom);
-			printf("location_bounds_get_rect_coords(left_top : %lf, %lf - right_bottom : %lf, %lf) \n",
+			printf("location_bounds_get_rect_coords(left_top: %lf, %lf - right_bottom: %lf, %lf) \n",
 			       left_top.latitude, left_top.longitude, right_bottom.latitude, right_bottom.longitude);
 		} else if (type == LOCATION_BOUNDS_POLYGON) {
 			location_bounds_foreach_polygon_coords(bounds, __poly_coords_cb, NULL);
@@ -164,10 +189,10 @@ void location_bounds_test()
 	if (ret != LOCATIONS_ERROR_NONE) {
 		printf("location_bounds_get_circle_coords() failed\n");
 	} else
-		printf("location_bounds_get_circle_coords(center : %lf, %lf, radius : %lf) \n", center2.latitude,
+		printf("location_bounds_get_circle_coords(center: %lf, %lf, radius: %lf) \n", center2.latitude,
 		       center2.longitude, radius2);
 
-	//Add the rect bounds
+	/*Add the rect bounds */
 	location_coords_s left_top;
 	left_top.latitude = 30;
 	left_top.longitude = 30;
@@ -196,10 +221,10 @@ void location_bounds_test()
 	if (ret != LOCATIONS_ERROR_NONE) {
 		printf("location_bounds_get_rect_coords() failed\n");
 	} else
-		printf("location_bounds_get_rect_coords(left_top : %lf, %lf - right_bottom : %lf, %lf) \n", left_top2.latitude,
+		printf("location_bounds_get_rect_coords(left_top: %lf, %lf - right_bottom: %lf, %lf) \n", left_top2.latitude,
 		       left_top2.longitude, right_bottom2.latitude, right_bottom2.longitude);
 
-	//Add the polygon bounds
+	/*Add the polygon bounds */
 
 	int poly_size = 3;
 	location_coords_s coord_list[poly_size];
@@ -238,7 +263,7 @@ void location_bounds_test()
 	else
 		printf("location_bounds_contains_coordinates() retrun FALSE \n");
 
-	//print current bounds
+	/*print current bounds */
 	ret = location_manager_foreach_boundary(manager, __location_bounds_cb, (void *)manager);
 	if (ret != LOCATIONS_ERROR_NONE) {
 		printf("location_manager_foreach_boundary() failed\n");
@@ -258,82 +283,170 @@ void location_get_last_information_test()
 
 	ret = location_manager_get_last_position(manager, &altitude, &latitude, &longitude, &timestamp);
 	if (ret != LOCATIONS_ERROR_NONE) {
-		printf(" Fail : location_manager_get_last_position ---> %d \n", ret);
+		printf(" Fail: location_manager_get_last_position ---> %d \n", ret);
 	} else {
 		printf("[%ld] alt: %g, lat: %g, long: %g\n", timestamp, altitude, latitude, longitude);
 	}
 
 	ret = location_manager_get_last_velocity(manager, &climb, &direction, &speed, &timestamp);
 	if (ret != LOCATIONS_ERROR_NONE) {
-		printf(" Fail : location_manager_get_last_velocity ---> %d \n", ret);
+		printf(" Fail: location_manager_get_last_velocity ---> %d \n", ret);
 	} else {
 		printf("climb: %f, direction: %f, speed: %f\n", climb, direction, speed);
 	}
 
 	ret = location_manager_get_last_accuracy(manager, &level, &horizontal, &vertical);
 	if (ret != LOCATIONS_ERROR_NONE) {
-		printf(" Fail : location_manager_get_last_accuracy ---> %d \n", ret);
+		printf(" Fail: location_manager_get_last_accuracy ---> %d \n", ret);
 	} else {
-		printf("Level : %d, horizontal: %g, vertical : %g\n", level, horizontal, vertical);
+		printf("Level: %d, horizontal: %g, vertical: %g\n", level, horizontal, vertical);
 	}
 
 	ret = gps_status_get_last_satellite(manager, &num_of_active, &num_of_inview, &timestamp);
 	if (ret != LOCATIONS_ERROR_NONE) {
-		printf(" Fail : gps_status_get_last_satellite_count_in_view ---> %d \n", ret);
+		printf(" Fail: gps_status_get_last_satellite_count_in_view ---> %d \n", ret);
 	} else {
-		printf("[%ld] Satellite number of active : %d, in view : %d\n", timestamp, num_of_active, num_of_inview);
+		printf("[%ld] Satellite number of active: %d, in view: %d\n", timestamp, num_of_active, num_of_inview);
 	}
 
 	ret = gps_status_foreach_last_satellites_in_view(manager, last_satellites_foreach_cb, NULL);
 	if (ret != LOCATIONS_ERROR_NONE) {
-		printf(" Fail : gps_status_foreach_last_satellites_in_view ---> %d \n", ret);
+		printf(" Fail: gps_status_foreach_last_satellites_in_view ---> %d \n", ret);
 	}
 }
+#endif
 
-int location_test()
+void location_cb(int error, double latitude, double longitude, double altitude, time_t timestamp, double speed, double climb, double direction, void *user_data)
 {
-	int ret;
-	ret = location_manager_create(LOCATIONS_METHOD_GPS, &manager);
-	printf("create : %d\n", ret);
+	printf("error[%d]\n", error);
+	printf("location_cb: lat[%f] lon[%f] alt[%f]\n", latitude, longitude, altitude);
+	printf("speed[%f] climb[%f] direction[%f]\n", speed, climb, direction);
 
-	location_bounds_test();
-	location_get_last_information_test();
-
-	//set zone changed callback
-	ret = location_manager_set_zone_changed_cb(manager, zone_event_cb, (void *)manager);
-	printf("set zone callback : %d\n", ret);
-
-	ret = location_manager_set_service_state_changed_cb(manager, _state_change_cb, (void *)manager);
-	printf("set state callback : %d\n", ret);
-
-	ret = location_manager_start(manager);
-	printf("start :  %d\n", ret);
-	return 1;
+	g_timeout_add_seconds(1, wait_test, NULL);
 }
 
-static GMainLoop *g_mainloop = NULL;
-
-static gboolean exit_program(gpointer data)
+static void __setting_cb(location_method_e method, bool enable, void *user_data)
 {
-	if (manager == NULL) {
-		printf("manager == NULL \n");
+	printf("method[%d], enable[%d]\n", method, enable);
+}
+
+static void select_menu()
+{
+	char buf[256];
+	int len = 0;
+	char *str = NULL;
+
+	str = fgets(buf, 255, stdin);
+	if (NULL == str) {
+		printf("fgets return NULL. \n");
+	}
+	len = g_utf8_strlen(buf, -1);
+	buf[len - 1] = '\0';
+
+	g_free(menu);
+	menu = g_strdup(buf);
+}
+
+static void print_menu()
+{
+	printf("==== LOCATION TEST ======\n");
+	printf("[1] LOCATIONS_METHOD_HYBRID: Get location\n");
+	printf("[2] LOCATIONS_METHOD_GPS: Get location\n");
+	printf("[3] LOCATIONS_METHOD_WPS: Get location\n");
+	printf("[4] LOCATIONS_METHOD_HYBRID: Single location\n");
+	printf("[5] LOCATIONS_METHOD_GPS: Single location\n");
+	printf("[x] Exit!!!\n\n");
+	printf("Select menu: ");
+}
+
+static int location_test()
+{
+	int ret = LOCATIONS_ERROR_NONE;
+
+	printf("==== LOCATION STATUS ======\n");
+	bool is_enabled = FALSE;
+	location_manager_is_enabled_method(LOCATIONS_METHOD_HYBRID, &is_enabled);
+	printf("hybrid: %d, ", is_enabled);
+
+	location_manager_is_enabled_method(LOCATIONS_METHOD_GPS, &is_enabled);
+	printf("gps: %d, ", is_enabled);
+
+	location_manager_is_enabled_method(LOCATIONS_METHOD_WPS, &is_enabled);
+	printf("wps: %d\n", is_enabled);
+
+	location_manager_set_setting_changed_cb(LOCATIONS_METHOD_GPS, __setting_cb, NULL);
+	location_manager_set_setting_changed_cb(LOCATIONS_METHOD_WPS, __setting_cb, NULL);
+
+	print_menu();
+	select_menu();
+
+	if (strcmp(menu, "1") == 0) {
+		ret = location_manager_create(LOCATIONS_METHOD_HYBRID, &manager);
+		printf("LOCATIONS_METHOD_HYBRID create: %d\n", ret);
+		ret = location_manager_start(manager);
+		printf("start: 	%d\n", ret);
+	} else if (strcmp(menu, "2") == 0) {
+		ret = location_manager_create(LOCATIONS_METHOD_GPS, &manager);
+		printf("LOCATIONS_METHOD_GPS create: %d\n", ret);
+		ret = location_manager_start(manager);
+		printf("start: 	%d\n", ret);
+	} else if (strcmp(menu, "3") == 0) {
+		ret = location_manager_create(LOCATIONS_METHOD_WPS, &manager);
+		printf("LOCATIONS_METHOD_WPS create: %d\n", ret);
+	} else if (strcmp(menu, "4") == 0) {
+		ret = location_manager_create(LOCATIONS_METHOD_HYBRID, &manager);
+		printf("LOCATIONS_METHOD_HYBRID create: %d\n", ret);
+		ret = location_manager_request_single_location(manager, 60, location_cb, manager);
+		printf("single_location create: %d\n", ret);
+	} else if (strcmp(menu, "5") == 0) {
+		ret = location_manager_create(LOCATIONS_METHOD_GPS, &manager);
+		printf("LOCATIONS_METHOD_HYBRID create: %d\n", ret);
+		ret = location_manager_request_single_location(manager, 60, location_cb, manager);
+		printf("single_location create: %d\n", ret);
+	} else if (strcmp(menu, "x") == 0) {
+		g_timeout_add_seconds(0.1, exit_program, NULL);
+		return 0;
 	} else {
-		int ret = location_manager_stop(manager);
-		printf("stop :  %d\n", ret);
-		ret = location_manager_destroy(manager);
-		printf("destroy :  %d\n", ret);
+		printf("Exit!!! Input: %s\n", menu);
+		g_timeout_add_seconds(0.1, exit_program, NULL);
+		return 0;
 	}
-	g_main_loop_quit(g_mainloop);
-	printf("Quit g_main_loop\n");
-	return FALSE;
+
+	if (ret != LOCATIONS_ERROR_NONE) {
+		printf("Test Failed!!! [%d]\n", ret);
+		g_timeout_add_seconds(0.1, exit_program, NULL);
+		return 0;
+	}
+	ret = location_manager_set_service_state_changed_cb(manager, _state_change_cb, (void *)manager);
+	printf("set state callback: %d\n", ret);
+
+	/* location_bounds_test(); */
+	/* set zone changed callback */
+	/*
+	ret = location_manager_set_zone_changed_cb(manager, zone_event_cb, (void *)manager);
+	printf("set zone callback: %d\n", ret);
+	*/
+	return 0;
+}
+
+static void location_cleanup()
+{
+	if (manager != NULL) {
+		int ret = location_manager_stop(manager);
+		printf("stop: %d\n", ret);
+		ret = location_manager_destroy(manager);
+		printf("destroy: %d\n", ret);
+		manager = NULL;
+	}
+	location_manager_unset_setting_changed_cb(LOCATIONS_METHOD_GPS);
+	location_manager_unset_setting_changed_cb(LOCATIONS_METHOD_WPS);
 }
 
 int main(int argc, char **argv)
 {
-	g_setenv("PKG_NAME", "com.samsung.location-test", 1);
 	g_mainloop = g_main_loop_new(NULL, 0);
 	location_test();
-	g_timeout_add_seconds(90, exit_program, NULL);
 	g_main_loop_run(g_mainloop);
+	location_cleanup();
 	return 0;
 }
